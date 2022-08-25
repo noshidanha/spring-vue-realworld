45a2e19606278ff8744d4d284d2983e432d87392
Noel Saldanha <2033868162@qq.com>
2022-08-25 19:30:18 +0800

    Modularized SQL functionality to realworld-core

---
+++ realworld-core/src/main/resources/db/schema-mysql.sql
@@ -0,0 +1,49 @@
+CREATE TABLE IF NOT EXISTS `users` (
+  id VARCHAR(255) PRIMARY KEY,
+  username VARCHAR(255) UNIQUE,
+  password VARCHAR(255),
+  email VARCHAR(255) UNIQUE,
+  bio TEXT,
+  image VARCHAR(511)
+);
+
+CREATE TABLE IF NOT EXISTS `articles` (
+  id VARCHAR(255) PRIMARY KEY,
+  user_id VARCHAR(255),
+  slug VARCHAR(255) UNIQUE,
+  title VARCHAR(255),
+  description TEXT,
+  body TEXT,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS `article_favorites` (
+  article_id VARCHAR(255) NOT NULL,
+  user_id VARCHAR(255) NOT NULL,
+  PRIMARY KEY(article_id, user_id)
+);
+
+CREATE TABLE IF NOT EXISTS `follows` (
+  user_id VARCHAR(255) NOT NULL,
+  follow_id VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `tags` (
+  id VARCHAR(255) PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `article_tags` (
+  article_id VARCHAR(255) NOT NULL,
+  tag_id VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `comments` (
+  id VARCHAR(255) PRIMARY KEY,
+  body TEXT,
+  article_id VARCHAR(255),
+  user_id VARCHAR(255),
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
